5ff84f3809c6a7a2b3c61c7dae8b38ff21be1bb1
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-23 16:02:03 +0000

    started on the safari app and learned a bit about OOP

---
+++ SafariPark/SafariParkApp/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace SafariParkApp {
+    public class Person {
+        private string _firstName = "";
+        private string _lastName = "";
+        private int _age;
+
+        public Person( string firstName ) {
+            _firstName = firstName;
+        }
+        public int Age {
+            get {
+                return _age;
+            }
+            set {
+                if ( Age < 0 ) {
+                    throw new ArgumentException();
+                }
+                _age = value;
+            }
+        }
+
+        public Person( string firstName, string lastName, int age ) {
+            _firstName = firstName;
+            _lastName = lastName;
+            Age = age;
+        }
+
+
+        public string FullName => $"{_firstName} {_lastName}";
+    }
+}
---
+++ SafariPark/SafariParkApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SafariParkApp {
+    internal class Program {
+        static void Main( string[] args ) {
+            Person yasir = new Person( "Yasir", "Ibrahim", 29 );
+            Console.WriteLine( yasir.FullName );
+
+            Person laura = new Person( "Laura" );
+            Console.WriteLine( laura );
+
+            yasir.Age = 30;
+            Console.WriteLine( $"Yasir is actually {yasir.Age}." );
+
+        }
+    }
+}
