afb96013ae5eb747e63e414e2e70484e475bf951
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-25 14:48:30 +0000

    makeFive and makeList methods working and all tests passing

--- Collections_Lab/Collections_Lib/ListExercises.cs
+++ Collections_Lab/Collections_Lib/ListExercises.cs
@@ -1,21 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace Collections_Lib
-{
-    public class ListExercises
-    {
+namespace Collections_Lib {
+    public class ListExercises {
         // returns a list of all the integers between 1 to max inclusive
         // that are multiples of 5
-        public static List<int> MakeFiveList(int max)
-        {
-            throw new NotImplementedException();
+        public static List<int> MakeFiveList( int max ) {
+            var list = new List<int>();
+            for ( int i = 1; i <= max; i++ ) {
+                if ( i % 5 == 0 ) {
+                    list.Add( i );
+                }
+            }
+            return list;
         }
 
         // returns a list of all the strings in sourceList that start with the letter 'A' or 'a'
-        public static List<string> MakeAList(List<string> sourceList)
-        {
-            throw new NotImplementedException();
+        public static List<string> MakeAList( List<string> sourceList ) {
+            var list = new List<string>();
+            foreach ( string s in sourceList ) {
+                if ( s.StartsWith( "A" ) || s.StartsWith( "a" ) ) {
+                    list.Add( s );
+                }
+            }
+            return list;
         }
     }
 }
