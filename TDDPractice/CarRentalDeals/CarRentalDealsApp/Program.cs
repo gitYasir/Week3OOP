a8978547344b46eb61af06342fa17ac2c7e63d11
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-27 22:14:58 +0000

    methods written and all tests passing

--- TDDPractice/CarRentalDeals/CarRentalDealsApp/Program.cs
+++ TDDPractice/CarRentalDeals/CarRentalDealsApp/Program.cs
@@ -5,7 +5,25 @@
         }
 
         public static string RentalSaving( int numOfDays ) {
-            return "";
+            var rentPerDay = 40;
+            var dicountThreeToSixDays = 20;
+            var dicountSevenDaysPlus = 50;
+            if ( numOfDays > 0 && numOfDays <= 2 ) {
+                return "For 2 days or less, you make no saving.";
+            }
+            else if ( numOfDays >= 3 && numOfDays <= 6 ) {
+                var orginal = numOfDays * rentPerDay;
+                var newPrice = orginal - dicountThreeToSixDays;
+                return $"Original price is £{orginal}. Final price is £{newPrice}. You save £20.";
+            }
+            else if ( numOfDays >= 7 ) {
+                var orginal = numOfDays * rentPerDay;
+                var newPrice = orginal - dicountSevenDaysPlus;
+                return $"Original price is £{orginal}. Final price is £{newPrice}. You save £50.";
+            }
+            else {
+                return "Number of days must be 1 or more.";
+            }
         }
 
     }
--- TDDPractice/CarRentalDeals/CarRentalDealsTests/RentalSavingShould.cs
+++ TDDPractice/CarRentalDeals/CarRentalDealsTests/RentalSavingShould.cs
@@ -3,12 +3,13 @@ using CarRentalDealsApp;
 namespace CarRentalDealsTests {
     public class RentalSavingShould {
 
-        [TestCase( 1, "For 1 day, you make no saving." )]
-        [TestCase( 2, "For 2 days, you make no saving." )]
+        [TestCase( 2, "For 2 days or less, you make no saving." )]
         [TestCase( 3, "Original price is £120. Final price is £100. You save £20." )]
         [TestCase( 6, "Original price is £240. Final price is £220. You save £20." )]
         [TestCase( 7, "Original price is £280. Final price is £230. You save £50." )]
         [TestCase( 10, "Original price is £400. Final price is £350. You save £50." )]
+        [TestCase( 0, "Number of days must be 1 or more." )]
+        [TestCase( -2, "Number of days must be 1 or more." )]
         public void GivenANumberOfDays_RentalSaving_ReturnsCorrectString( int days, string expected ) {
             Assert.That( Program.RentalSaving( days ), Is.EqualTo( expected ) );
         }
