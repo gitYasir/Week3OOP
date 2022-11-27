61f67942faa72aea6d2f94f0445a7b6e64b50886
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-27 21:00:41 +0000

    started TDD practice

---
+++ TDDPractice/PlayingBanjo/PlayingBanjoTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace PlayingBanjoTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
